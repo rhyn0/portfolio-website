cb73743766b04e3eff3635e09ed1bd4c31210b92
Ryan Ozawa <ryan.ozawa21@gmail.com>
2023-11-19 13:39:35 -0800

    add: projects main page

---
+++ app/projects/page.tsx
@@ -0,0 +1,16 @@
+export default function ProjectsPage() {
+   return (
+      <div className="container mx-auto">
+         <div className="grid grid-cols-2 gap-4 md:grid-cols-2">
+            <div className="flex h-96 flex-col items-center justify-center">
+               <div className="h-1/2 w-1/2 animate-pulse rounded-full bg-gray-200"></div>
+               <div className="h-1/2 w-1/2 animate-pulse rounded-full bg-gray-200"></div>
+            </div>
+            <div className="flex h-96 flex-col items-center justify-center">
+               <div className="h-1/2 w-1/2 animate-pulse rounded-full bg-gray-200"></div>
+               <div className="h-1/2 w-1/2 animate-pulse rounded-full bg-gray-200"></div>
+            </div>
+         </div>
+      </div>
+   );
+}
